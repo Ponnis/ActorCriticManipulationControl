dcf02dc1c84dd985edd087a5d206520262945a25
mmguberina <guberina@student.chalmers.se>
2021-05-06 16:44:39 +0200

    synching purposes

--- make_simple_env.py
+++ make_simple_env.py
@@ -3,6 +3,7 @@ import gym
 
 
 env = gym.make('FetchReach-v1')
+#env = gym.make('FetchPickAndPlace-v1')
 env.render()
 obs = env.reset()
 done = False
@@ -12,17 +13,18 @@ def policy(observation, desired_goal):
     # we just sample random actions.
     return env.action_space.sample()
 
-while not done:
-    env.render()
-    action = policy(obs['observation'], obs['desired_goal'])
-    obs, reward, done, info = env.step(action)
-
-    # If we want, we can substitute a goal here and re-compute
-    # the reward. For instance, we can just pretend that the desired
-    # goal was what we achieved all along.
-    substitute_goal = obs['achieved_goal'].copy()
-    substitute_reward = env.compute_reward(
-        obs['achieved_goal'], substitute_goal, info)
-    print('reward is {}, substitute_reward is {}'.format(
-        reward, substitute_reward))
-
+for i in range(20):
+    while not done:
+        env.render()
+        action = policy(obs['observation'], obs['desired_goal'])
+        obs, reward, done, info = env.step(action)
+    
+        # If we want, we can substitute a goal here and re-compute
+        # the reward. For instance, we can just pretend that the desired
+        # goal was what we achieved all along.
+        substitute_goal = obs['achieved_goal'].copy()
+        substitute_reward = env.compute_reward(
+            obs['achieved_goal'], substitute_goal, info)
+        print('reward is {}, substitute_reward is {}'.format(
+            reward, substitute_reward))
+    
